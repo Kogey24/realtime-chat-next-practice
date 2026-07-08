ff058e912b589b96a26f945216210547cc1ae61f
Harmmerton Kogi <hammertonkogey@gmail.com>
2026-07-08 15:52:41 +0300

    feat: Connected upstash provider for redis and connected a database successfully

--- app/api/[[...slugs]]/route.ts
+++ app/api/[[...slugs]]/route.ts
@@ -1,10 +1,23 @@
+import { redis } from '@/lib/redis';
 import { Elysia } from 'elysia'
+import { nanoid } from 'nanoid'
 
+
+const ROOM_TTL_SECONDS = 60 * 10
 
 export const rooms = new Elysia({ prefix: "/room" })
-    .post("/create", () => {
-        console.log("Room created");
- })
+    .post("/create", async () => {
+        const roomId = nanoid();
+
+        await redis.hset(`meta:${roomId}`, {
+            connected: [],
+            createdAt: Date.now(),
+        })
+
+        await redis.expire(`meta:${roomId}`, ROOM_TTL_SECONDS)
+
+        return { roomId }
+    })
 
 export const app = new Elysia({ prefix: '/api' })
     .use(rooms)
